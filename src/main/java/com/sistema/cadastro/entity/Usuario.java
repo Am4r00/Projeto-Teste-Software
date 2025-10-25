aa6b555cb1690d144bc33b112439f7912c5621f8
victor hugo <victorvilto@gmail.com>
2025-10-25 13:42:19 -0300

    feat(usuario): adicionar entidade usuario

---
+++ src/main/java/com/sistema/cadastro/entity/Usuario.java
@@ -0,0 +1,17 @@
+package com.sistema.cadastro.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+}
