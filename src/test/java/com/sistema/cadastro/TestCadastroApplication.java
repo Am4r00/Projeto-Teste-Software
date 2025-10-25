8149520eb7c57110913827990d27086386515a6f
victor hugo <victorvilto@gmail.com>
2025-10-25 13:28:14 -0300

    First commit

---
+++ src/test/java/com/sistema/cadastro/TestCadastroApplication.java
@@ -0,0 +1,11 @@
+package com.sistema.cadastro;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CadastroApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
